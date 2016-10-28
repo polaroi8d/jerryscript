1f75cd65ae57c7bf15228de0177d5eaff104a55d
Levente Orban <orbanl@inf.u-szeged.hu>
2016-10-28 10:57:35 +0200

    Change the JERRY_INIT_DEBUGGER flag instead of JERRY_INIT_LEXICAL_ENV
    JerryScript-DCO-1.0-Signed-off-by: Levente Orban orbanl@inf.u-szeged.hu

--- jerry-core/parser/js/js-parser-statm.c
+++ jerry-core/parser/js/js-parser-statm.c
@@ -1558,13 +1558,13 @@ parser_parse_statements (parser_context_t *context_p) /**< context */
   parser_stack_push_uint8 (context_p, PARSER_STATEMENT_START);
   parser_stack_iterator_init (context_p, &context_p->last_statement);
 
-#ifdef DEBUGGER
+#ifdef JERRY_DEBUGGER
   /* Set lexical enviroment for the debugger*/
-  if (JERRY_INIT_LEXICAL_ENV)
+  if (JERRY_CONTEXT (jerry_init_flags) & JERRY_INIT_LEXICAL_ENV)
   {
     context_p->status_flags |= PARSER_LEXICAL_ENV_NEEDED;
   }
-#endif /* DEBUGGER */
+#endif /* JERRY_DEBUGGER */
 
   while (context_p->token.type == LEXER_LITERAL
          && context_p->token.lit_location.type == LEXER_STRING_LITERAL)
