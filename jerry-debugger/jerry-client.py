de2c7b7a457e96ec6d279f0c064367f1fbc6347f
Levente Orban <orbanl@inf.u-szeged.hu>
2016-11-07 09:20:44 +0100

    Add jerry-client.py
    JerryScript-DCO-1.0-Signed-off-by: Levente Orban orbanl@inf.u-szeged.hu

---
+++ jerry-debugger/jerry-client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright 2016 University of Szeged.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import socket
+import sys
+
+PORT = 5001
+HOST = "localhost"
+
+def main():
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((HOST, PORT))
+    except socket.error, msg:
+        print('Failed to create the socket. Error code: %d, Error message: %s' % (msg[0], msg[1]))
+        sys.exit()
+
+    print ('Socket created on: %d'% PORT)
+
+    data = client_socket.recv(512)
+    print('Received: %s' % (data))
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
