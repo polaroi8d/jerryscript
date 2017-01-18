0018f1acd6436973beb47163632295bc88118e08
Levente Orban <orbanl@inf.u-szeged.hu>
2017-01-18 10:31:23 +0100

    Fix some CMake deletion fault (#19)
    - Fix the ordering of the message types
     - Fix some style issue
    
    JerryScript-DCO-1.0-Signed-off-by: Levente Orban orbanl@inf.u-szeged.hu

--- jerry-core/debugger/jerry-debugger.h
+++ jerry-core/debugger/jerry-debugger.h
@@ -62,9 +62,9 @@ typedef enum
   JERRY_DEBUGGER_BYTE_CODE_CPTR = 2,                  /**< byte code compressed pointer */
   JERRY_DEBUGGER_PARSE_FUNCTION = 3,                  /**< parsing a new function */
   JERRY_DEBUGGER_BREAKPOINT_LIST = 4,                 /**< there is more piece of the breakpoint list */
-  JERRY_DEBUGGER_SOURCE_FILE_NAME = 6,                /**< source file name fragment */
-  JERRY_DEBUGGER_FUNCTION_NAME = 7,                   /**< function name fragment */
-  JERRY_DEBUGGER_FREE_BYTE_CODE_CPTR = 8,             /**< invalidate byte code compressed pointer */
+  JERRY_DEBUGGER_SOURCE_FILE_NAME = 5,                /**< source file name fragment */
+  JERRY_DEBUGGER_FUNCTION_NAME = 6,                   /**< function name fragment */
+  JERRY_DEBUGGER_FREE_BYTE_CODE_CPTR = 7,             /**< invalidate byte code compressed pointer */
 } jerry_debugger_header_type_t;
 
 /**
